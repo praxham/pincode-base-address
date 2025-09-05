b4ec4bf2ad65f9e3d1a6d895e44d49a0330bd3cc
praxham <prathampatankar1234@gmail.com>
2025-09-05 11:40:43 +0530

    pincode to base adress working

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/api/v1/fromPincode/getBaseAddress/:pincode", async (c) => {
+  const pincode = c.req.param("pincode");
+  let district;
+  let state;
+  let areas;
+  let country;
+  try {
+    const response = await fetch(
+      `https://api.postalpincode.in/pincode/${pincode}`
+    );
+    if (!response.ok) {
+      return c.json({ error: "Failed to fetch pincode details" }, 500);
+    }
+
+    const data = await response.json();
+    console.log(data[0].PostOffice);
+    if (data[0].Status === "Success" && data[0].PostOffice.length > 0) {
+      let baseAddresses: string[] = [];
+      data[0]?.PostOffice?.map(
+        (
+          areas: {
+            Name: string;
+            District: string;
+            State: string;
+            Country: string;
+          },
+          index: number
+        ) => {
+          const { District, State, Name, Country } = areas;
+          baseAddresses.push(
+            `${Name}, ${District}-${pincode}, ${State}, ${Country}`
+          );
+        }
+      );
+      const { District, State, Country } = data[0].PostOffice[0];
+      district = District;
+      state = State;
+      areas = data[0]?.PostOffice?.map((areas: { Name: string }) => {
+        return areas?.Name;
+      });
+      country = Country;
+      return c.json({ baseAddresses, district, state, areas, country });
+    } else {
+      return c.json({ error: "Pincode not found or invalid" }, 404);
+    }
+  } catch (error: any) {
+    console.error("Error:", error.message);
+    return c.json({ error: "Internal server error" }, 500);
+  }
+});
+
+export default app;
